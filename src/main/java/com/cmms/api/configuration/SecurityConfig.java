1e91581432af7450ab273e18cf8f996239e799f8
Roberto Angulo <89711998+RobertoAnguloDeveloper@users.noreply.github.com>
2024-01-31 20:54:17 -0500

    JWT Tested

--- src/main/java/com/cmms/api/configuration/SecurityConfig.java
+++ src/main/java/com/cmms/api/configuration/SecurityConfig.java
@@ -10,14 +10,14 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+// import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+// import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -57,14 +57,20 @@ public class SecurityConfig {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-				.authorizeHttpRequests(req -> req
-                //.requestMatchers("/login/**").permitAll()
-                .requestMatchers("/api/user/*").hasAnyAuthority("Superuser", "Site manager")
-                .anyRequest().authenticated()).httpBasic(withDefaults())
-				.userDetailsService(myUserDetailsService)
-				.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-		return http.build();
+            .authorizeHttpRequests(req -> req
+                .requestMatchers("/api/user/login").permitAll()  // Permitir acceso sin autenticación a rutas específicas
+                .requestMatchers("/api/user/save").hasAnyAuthority("Superuser", "Site manager")
+				.requestMatchers("/api/user/technicians").hasAnyAuthority("Superuser", "Site manager")
+				.requestMatchers("/api/user/supervisors").hasAnyAuthority("Superuser", "Site manager")
+				.requestMatchers("/api/user/site-managers").hasAnyAuthority("Superuser", "Site manager")
+				.requestMatchers("/api/user/update").hasAnyAuthority("Superuser", "Site manager")
+				.requestMatchers("/api/user/{id}").hasAnyAuthority("Superuser", "Site manager")
+                .anyRequest().authenticated())
+            .userDetailsService(myUserDetailsService)
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+			return http.build();
 	}
 
 	@Bean
--- src/main/java/com/cmms/api/controllers/UserController.java
+++ src/main/java/com/cmms/api/controllers/UserController.java
@@ -1,13 +1,20 @@
 package com.cmms.api.controllers;
 
+// import com.cmms.api.auth.request.AuthRequest;
 import com.cmms.api.auth.response.AuthResponse;
 import com.cmms.api.auth.service.AuthService;
 import com.cmms.api.models.SecurityUser;
 import com.cmms.api.models.User;
+// import com.cmms.api.services.JpaUserDetailsService;
+import com.cmms.api.services.JwtService;
 import com.cmms.api.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+// import org.springframework.security.access.prepost.PreAuthorize;
+// import org.springframework.security.authentication.AuthenticationManager;
+// import org.springframework.security.authentication.BadCredentialsException;
+// import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+// import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,6 +31,15 @@ public class UserController {
     @Autowired
     private AuthService authService;
 
+    @Autowired
+    JwtService jwtService;
+
+    // @Autowired
+    // private AuthenticationManager authenticationManager;
+
+    // @Autowired
+    // private JpaUserDetailsService myUserDetailService;
+
     // public UserController(AuthService authService){
     // this.authService = authService;
     // }
@@ -33,6 +49,18 @@ public class UserController {
         return ResponseEntity.ok(authService.authenticate(new SecurityUser(user)));
     }
 
+    // @PostMapping("/login")
+    // public AuthResponse createToken(@RequestBody AuthRequest authenticationRequest) throws Exception {
+    //     try {
+    //         UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+    //         authenticationManager.authenticate(authentication);
+    //     } catch (BadCredentialsException e) {
+    //         throw new Exception("Invalid username or password", e);
+    //     }
+    //     UserDetails userDetails = myUserDetailService.loadUserByUsername(authenticationRequest.getUsername());
+    //     String token = jwtService.generateToken(userDetails);
+    //     return new AuthResponse(token);
+    // }
 
     @PostMapping("/save")
     public ResponseEntity<User> createUser(@RequestBody User user) {
